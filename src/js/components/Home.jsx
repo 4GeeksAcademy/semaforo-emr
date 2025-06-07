659d3b5e179db876fc3b06f8f2358dcd95184e16
ericamoratilla <erica.moratilla@gmail.com>
2025-06-07 17:20:06 +0000

    First semaforo commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,50 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	// Para seleccionar el estado del color actual
+
+	const [color, setColor] = useState("red");
+
+	// Array para crear el botón que cambie de color
+
+	const [colors, setColors] = useState(["red", "yellow", "green"]);
+
+	// Función para cambiar de color
+
+	const nextColor = () => {
+		const currentIndex = colors.indexOf(color);
+		const nextIndex = (currentIndex + 1) % colors.length;
+		setColor (colors[nextIndex]);
+	};
+
+	// Función para añadir el morado
+
+	const addPurple = () => {
+		if (!colors.includes("purple")){
+			setColors([...colors,"purple"]);
+		}
+	};
+
+	return (
+	<div className="traffic-master">
+      <div className="traffic-light">
+        {colors.map((c) => (
+          <div
+            key={c}
+            className={`light ${c} ${color === c ? "shining" : ""}`}
+            onClick={() => setColor(c)}
+          ></div>
+        ))}
+      </div>
+	   <div className="buttons btn btn-dark">
+        <button className="btn btn-light" onClick={nextColor}>Otro color</button>
+        <button className="btn btn-light" onClick={addPurple}>¡Quiero morado!</button>
+      </div>
+    </div>
+  );
 };
+
 
 export default Home;
